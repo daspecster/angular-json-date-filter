e5569d0fddd10943309fefeac65ac17657e74efb
Thomas Schultz <daspecster@gmail.com>
2013-05-10 12:32:27 -0300

    Moment.js support (John Pedrie), bail if input is undefined.

--- jsonDate.js
+++ jsonDate.js
@@ -5,10 +5,27 @@
 // This filter takes a .NET JSON encoded date and makes it readable...
 // i.e.  /Date(-795988800000-0400)/ turns into 10/11/1944
 // to use in your template just do.... {{ mydata.somedate | jsonDate }}
-filters.filter('jsonDate', function() {
-  return function(input) {
+// If moment.js is included in your project, you can pass an optional
+// second parameter "format" with a moment.js-compatible date format string.
+// If the second parameter is empty or moment is not included, we'll fall back to default.
 
+filters.filter('jsonDate', function() {
+  return function(input, format) {
+    
+    // Exit if the value isn't defined
+    if(angular.isUndefined(input)) {
+      return;
+    }
+    
     var date = new Date(parseInt(input.substr(6)));
-    return date.toLocaleDateString();
+
+    // John Pedrie added Moment.js support
+    if(typeof moment !== 'undefined' && format) {
+      var momentObj = moment(date);
+      return momentObj.format(format);
+    }
+    else {
+      return date.toLocaleDateString();
+    }
   }
 });
